cf3a7057be494fcd98ffe6435fae8965d7bb6ad2
rafaeljacobsen <rafaeljacobsen05@gmail.com>
2022-04-03 16:47:11 -0400

    first commit

---
+++ spanishconjugations.py
@@ -0,0 +1,272 @@
+people = ["yo","tu","el","nosotros","ellos"]
+presentAr = {"yo":"o","tu":"as","el":"a","nosotros":"amos","ellos":"an"}
+presentEr = {"yo":"o","tu":"es","el":"e","nosotros":"emos","ellos":"en"}
+presentIr = {"yo":"o","tu":"es","el":"e","nosotros":"imos","ellos":"en"}
+preteriteAr = {"yo":"é","tu":"aste","el":"ó","nosotros":"amos","ellos":"aron"}
+preteriteEr = {"yo":"í","tu":"iste","el":"ió","nosotros":"imos","ellos":"ieron"}
+preteriteIrr = {"yo":"e","tu":"iste","el":"o","nosotros":"imos","ellos":"ieron"}
+subjunctiveAr = {"yo":"e","tu":"es","el":"e","nosotros":"emos","ellos":"en"}
+subjunctiveEr = {"yo":"a","tu":"as","el":"a","nosotros":"amos","ellos":"an"}
+imperfectAr = {"yo":"aba","tu":"abas","el":"aba","nosotros":"ábamos","ellos":"aban"}
+imperfectEr = {"yo":"ía","tu":"ías","el":"ía","nosotros":"íamos","ellos":"ían"}
+preteriteIrregularRoot = {"tener":"tuv","estar":"estuv","decir":"dij","caber":"cup","hacer":"hic","ander":"anduv","poder":"pud","saber":"sup","traer":"traj","poner":"pus","querer":"quis","venir":"vin","traducir":"traduj"}
+serIrPreterite = {"yo":"fui","tu":"fuiste","el":"fue","nosotros":"fuimos","ellos":"fueron"}
+darPreterite = {"yo":"di","tu":"diste","el":"dio","nosotros":"dimos","ellos":"dieron"}
+sonreirPreterite = {"yo":"sonreí","tu":"sonreíste","el":"sonrió","nosotros":"sonreímos","ellos":"sonrieron"}
+oirPreterite = {"yo":"oí","tu":"oíste","el":"oyó","nosotros":"oímos","ellos":"oyeron"}
+freirPreterite = {"yo":"freí","tu":"freíste","el":"frio","nosotros":"freímos","ellos":"frieron"}
+yoVerbs = {"decir":"digo","traducir":"traduzco","hacer":"hago","poner":"pongo","salir":"salgo",
+"jugar":"juego","tener":"tengo","traer":"traigo","seguir":"sigo","venir":"vengo","caber":"quepo","caer":"caigo","conocer":"conozco","dar":"doy","saber":"sé","valer":"valgo","ver":"veo","huir":"huyo"}
+serPresent = {"yo":"soy","tu":"eres","el":"es","nosotros":"somos","ellos":"son"}
+estarPresent = {"yo":"estoy","tu":"estás","el":"está","nosotros":"estamos","ellos":"están"}
+irPresent = {"yo":"voy","tu":"vas","el":"va","nosotros":"vamos","ellos":"van"}
+haberPresent = {"yo":"he","tu":"has","el":"ha","nosotros":"hemos","ellos":"han"}
+sonreirPresent = {"yo":"sonrío","tu":"sonríes","el":"sonríe","nosotros":"sonreímos","ellos":"sonríen"}
+oirPresent = {"yo":"oigo","tu":"oyes","el":"oye","nosotros":"oímos","ellos":"oyen"}
+freirPresent = {"yo":"frío","tu":"fríes","el":"fríe","nosotros":"freímos","ellos":"fríen"}
+serImperfect = {"yo":"era","tu":"eras","el":"era","nosotros":"erámos","ellos":"eran"}
+irImperfect = {"yo":"iba","tu":"ibas","el":"iba","nosotros":"íbamos","ellos":"iban"}
+verImperfect = {"yo":"veía","tu":"veías","el":"veía","nosotros":"veíamos","ellos":"veían"}
+presentPerfectIrrStem = {"decir":"dicho","hacer":"hecho","poner":"puesto","romper":"roto","abrir":"abierto","cubrir":"cubierto","escribir":"escrito","resolver":"resuelto","ver":"visto","volver":"vuelto","imprimir":"impreso","morir":"muerto","freir":"frito","sonreir":"sonreído","oir":"oído"}
+darSubjunctive = {"yo":"dé","tu":"des","el":"dé","nosotros":"demos","ellos":"den"}
+irSubjunctive = {"yo":"vaya","tu":"vayas","el":"vaya","nosotros":"vayamos","ellos":"vayan"}
+serSubjunctive = {"yo":"sea","tu":"seas","el":"sea","nosotros":"seamos","ellos":"sean"}
+haberSubjunctive = {"yo":"haya","tu":"hayas","el":"haya","nosotros":"hayamos","ellos":"hayan"}
+estarSubjunctive = {"yo":"esté","tu":"estés","el":"esté","nosotros":"estemos","ellos":"estén"}
+saberSubjunctive = {"yo":"sepa","tu":"sepas","el":"sepa","nosotros":"sepamos","ellos":"sepan"}
+etoieStems = ["pensar","advertir","acertar","alentar","apretar","arrendar","asentar","aterrar","atravesar","calentar","cegar","cerrar","comenzar","concertar","confesar","denegar","desalentar","desconcertar","desenterrar","deshelar","despertar","desplegar","desterrar","emparentar","empezar","encerrar","encomendar","recomenzar","enmendar","enterrar","gobernar","helar","invernar","manifestar","mentar","merendar","negar","nevar","quebrar","recalentar","recomendar","regar","remendar","renegar","restregar","reventar","salpimentar","segar","sembrar","sentar","serrar","sosegar","soterrar","temblar","tentar","tropezar","querer","ascender","atender","defender","desatender","descender","encender","entender","extender","perder","tender","trascender","verter","preferir","mentir","concernir","adherir","adquirir","diferir","digerir","herir","inferir","ingerir","interferir","preferir","referir","transferir","discernir","divertir","sentir","revertir","resentir","presentir","invertir","corregir","venir"]
+otoueStems = ["recordar", "acordar", "almorzar","apostar", "aprobar", "asolar", "colar", "comprobar", "concordar", "consolar", "contar", "costar", "demostrar", "desaprobar", "descontar", "encontrar", "engrosar", "mostrar", "poblar", "probar", "recostar", "renovar", "repoblar", "reprobar", "resonar", "rodar", "sobrevolar", "soldar", "soltar", "sonar", "soñar", "tostar", "tronar", "volar", "volcar", "poder", "absolver", "conmover", "demoler", "desenvolver", "devolver", "disolver", "doler", "envolver", "llover", "moler", "morder", "mover", "promover", "remorder", "remover", "resolver", "revolver", "morir", "dormir","volver"] 
+etoiStems = ["seguir","sonreír","conseguir","repetir","teñir","elegir","regir","pedir","derretir","despedir","freír","gemir","impedir","servir","repetir","rendir","reñir","medir","vestir","reelegir","colegir","perseguir"]
+miscStems = {"jugar":"jueg","oler":"huel","sonreir":"sonrí"}
+futureEndings = {"yo":"é","tu":"ás","el":"á","nosotros":"emos","ellos":"án"}
+futureIrregularAddD = ["tener","poner","salir","venir","valer"]
+futureIrregularDrop = ["poder","caber","querer","saber","haber"]
+futureIrregular={"decir":"dir","hacer":"har"}
+conditionalEndings = {"yo":"ía","tu":"ías","el":"ía","nosotros":"íamos","ellos":"ían"}
+subjunctive1Endings = {"yo":"ra","tu":"ras","el":"ra","nosotros":"ramos","ellos":"ran"}
+subjunctive2Endings = {"yo":"se","tu":"ses","el":"se","nosotros":"semos","ellos":"sen"}
+def present(verb, person):
+  if verb in yoVerbs and person == "yo":
+    return(yoVerbs[verb])
+  elif verb == "ser": return(serPresent[person])
+  elif verb == "estar": return(estarPresent[person])
+  elif verb == "ir": return(irPresent[person])
+  elif verb == "haber": return(haberPresent[person])  
+  elif verb == "sonreir": return(sonreirPresent[person]) 
+  elif verb == "oir": return(oirPresent[person])
+  elif verb == "freir": return(freirPresent[person]) 
+  elif verb == "reir": return(present("sonreir",person)[3:]) 
+  else:
+    if person == "nosotros":
+      root,ending = verb[:-2],verb[-2:]
+    else:
+      if verb in etoieStems:
+        root, ending = verb[:-2],verb[-2:]
+        root = root[:root.rfind("e")]+"ie"+root[root.rfind("e")+1:]
+      elif verb in otoueStems:
+        root, ending = verb[:-2],verb[-2:]
+        root = root[:root.rfind("o")]+"ue"+root[root.rfind("o")+1:]
+      elif verb in etoiStems:
+        root, ending = verb[:-2],verb[-2:]
+        root = root[:root.rfind("e")]+"i"+root[root.rfind("e")+1:]
+      elif verb in miscStems:
+        root, ending = miscStems[verb],verb[-2:]
+      else: 
+        root, ending = verb[:-2],verb[-2:]
+    if verb[-4:] == "guir" and person == "yo":
+      return(root[:-2]+"go")
+    elif verb[-3:] == "gir" or verb[-3:] == "ger":
+      if person == "yo": return(root[:-1]+"jo")
+    elif verb[-3:] == "uir" and person != "nosotros" and verb[-4:] != "guir":
+      return(root+"y"+presentIr[person])
+    if ending == "ar": return(root+presentAr[person])
+    elif ending == "er": return(root+presentEr[person])
+    elif ending == "ir": return(root+presentIr[person])
+    else: print("ERROR: are you sure you typed the verb correctly?")
+def preterite(verb, person):
+  if verb in preteriteIrregularRoot:
+    root, ending = preteriteIrregularRoot[verb],verb[-2:]
+    if verb == "hacer" and person == "el": return("hizo")
+    elif (root+preteriteIrr[person])[-6:] == "jieron": return((root+preteriteIrr[person])[:-6]+"jeron")
+    else: return(root+preteriteIrr[person])
+  elif verb == "ir" or verb == "ser": return(serIrPreterite[person])
+  elif verb == "dar": return(darPreterite[person])
+  elif verb == "sonreir": return(sonreirPreterite[person])
+  elif verb == "oir": return(oirPreterite[person])
+  elif verb == "freir": return(freirPreterite[person])
+  elif verb == "reir": return(preterite("sonreir",person)[3:]) 
+  elif verb[-3:] == "uir" and person == "yo" and verb[-4:] != "guir":
+    return(verb[:-2]+"i")
+  elif verb[-3:] == "uir" and person == "el" and verb[-4:] != "guir":
+    return(verb[:-2]+"yó")
+  elif verb[-3:] == "uir" and person == "ellos" and verb[-4:] != "guir":
+    return(verb[:-2]+"yeron")
+  elif verb[-3:] == "car" and person == "yo":
+    root, ending = verb[:-3]+"qu",verb[-2:]
+  elif verb[-3:] == "gar" and person == "yo":
+    root, ending = verb[:-2]+"u",verb[-2:]
+  elif verb[-3:] == "zar" and person == "yo":
+    root, ending = verb[:-3]+"c",verb[-2:]
+  else:
+    root, ending = verb[:-2],verb[-2:]
+
+  if person == "el" or person == "ellos":
+    if verb[-2:] == "ir":
+      if verb in etoiStems or verb in etoieStems:
+        root = root[:root.rfind("e")]+"i"+root[root.rfind("e")+1:]
+      if verb in otoueStems:
+        root = root[:root.rfind("o")]+"u"+root[root.rfind("o")+1:]
+  if ending == "ar": return(root+preteriteAr[person])
+  elif ending == "er" or ending == "ir": return(root+preteriteEr[person])
+  else: print("ERROR: are you sure you typed the verb correctly?")
+def imperfect(verb, person):
+  if verb == "ser": return(serImperfect[person])
+  elif verb == "ir": return(irImperfect[person])
+  elif verb == "ver": return(verImperfect[person])
+  else:
+    root, ending = verb[:-2],verb[-2:]
+    if ending == "ar": return(root+imperfectAr[person])
+    elif ending == "er" or ending == "ir": return(root+imperfectEr[person])
+    else: print("ERROR: are you sure you typed the verb correctly?")
+def presentPerfect(verb, person):
+  ending = verb[-2:]
+  if verb in presentPerfectIrrStem:
+    stem = presentPerfectIrrStem[verb]
+  else:
+    if ending == "ar": stem = verb[:-2]+"ado"
+    if ending == "er" or ending == "ir": stem = verb[:-2]+"ido"
+  return(haberPresent[person]+" "+stem)
+def pastPerfect(verb, person):
+  ending = verb[-2:]
+  if verb in presentPerfectIrrStem:
+    stem = presentPerfectIrrStem[verb]
+  else:
+    if ending == "ar": stem = verb[:-2]+"ado"
+    if ending == "er" or ending == "ir": stem = verb[:-2]+"ido"
+  return(imperfect("haber",person)+" "+stem)
+def subjunctive(verb, person):
+  if verb == "dar": return(darSubjunctive[person])
+  elif verb == "estar": return(estarSubjunctive[person])
+  elif verb == "saber": return(saberSubjunctive[person])
+  elif verb == "ser": return(serSubjunctive[person])
+  elif verb == "haber": return(haberSubjunctive[person])
+  elif verb == "ir": return(irSubjunctive[person])
+  else:
+    root, ending = present(verb, "yo")[:-1],verb[-2:]
+    if person == "nosotros":
+      if ending == "ir":
+        if verb == "sonreir": return("sonriamos")
+        if verb in etoieStems:
+          root = root[:root.rfind("ie")]+"i"+root[root.rfind("ie")+2:]
+        elif verb in otoueStems:
+          root = root[:root.rfind("ue")]+"u"+root[root.rfind("ue")+2:]
+        elif verb in etoiStems:
+          root = root[:root.rfind("i")]+"i"+root[root.rfind("i")+1:]
+        elif verb == miscStems:
+          root, ending = miscStems[verb],verb[-2:]
+      else:
+        if verb in etoieStems:
+          root = root[:root.rfind("ie")]+"e"+root[root.rfind("ie")+2:]
+        elif verb in otoueStems:
+          print("here")
+          root = root[:root.rfind("ue")]+"o"+root[root.rfind("ue")+2:]
+        elif verb in etoiStems:
+          root = root[:root.rfind("i")]+"e"+root[root.rfind("i")+1:]
+    if verb[-3:] == "car": root, ending = root[:-1]+"qu",verb[-2:]
+    elif verb[-3:] == "gar": root, ending = root[:-1]+"gu",verb[-2:]
+    elif verb[-3:] == "zar": root, ending = root[:-1]+"c",verb[-2:]
+    if ending == "ar": return(root+subjunctiveAr[person])
+    if ending == "er" or ending == "ir": return(root+subjunctiveEr[person])
+def future(verb, person):
+  if verb in futureIrregularAddD: stem = verb[:-2]+"dr"
+  elif verb in futureIrregularDrop: stem = verb[:-2]+"r"
+  elif verb in futureIrregular: stem = futureIrregular[verb]
+  else: stem = verb
+  return(stem+futureEndings[person])
+def conditional(verb, person):
+  if verb in futureIrregularAddD: stem = verb[:-2]+"dr"
+  elif verb in futureIrregularDrop: stem = verb[:-2]+"r"
+  elif verb in futureIrregular: stem = futureIrregular[verb]
+  else: stem = verb
+  return(stem+conditionalEndings[person])
+def imperfectSubjunctive(verb, person):
+  root = preterite(verb, "ellos")
+  if verb == "sonreir": root = "sonrieron"
+  if person == "nosotros":
+    if verb[-2:] == "ar":
+      return(root[:-4]+"áramos, "+root[:-4]+"ásemos")
+    else:
+      return(root[:-4]+"éramos, "+root[:-4]+"ésemos")
+  return(root[:-3]+subjunctive1Endings[person]+", "+root[:-3]+subjunctive2Endings[person])
+def imperativePos(verb, person, reflex):
+  if person == "tu":
+    return(present(verb, "el"))
+  elif person == "usted":
+    return(subjunctive(verb,"el"))
+  elif person == "ustedes":
+    return(subjunctive(verb,"ellos"))
+  elif person == "nosotros":
+    if reflex:
+      if verb[-2:] == "ar":
+        return(subjunctive(verb,"nosotros")[:-4]+"émo")
+      else:
+        return(subjunctive(verb,"nosotros")[:-4]+"ámo")
+    else:
+      return(subjunctive(verb,"nosotros"))
+def imperativeNeg(verb, person):
+  if person == "tu":
+    return(subjunctive(verb, "tu"))
+  elif person == "usted":
+    return(subjunctive(verb,"el"))
+  elif person == "ustedes":
+    return(subjunctive(verb,"ellos"))
+  elif person == "nosotros":
+    return(subjunctive(verb,"nosotros"))
+while True:
+  verb = input("Enter a verb to conjugate: ")
+  #person = input("Enter a person for the verb (yo, tú, él, nosotros, ellos): ")
+  print("Enter a tense (present, imperfect, preterite, present perfect, past perfect, subjunctive, future, conditional, imperfect subjunctive, affirmative imperative, negative imperative): ")
+  tense = input()
+  imperativePeople = ["tu","usted","nosotros","ustedes"]
+  if verb[-2:] == "se":
+    prefix = {"yo":"me ","tu":"te ","el":"se ","nosotros":"nos ","ellos":"se ","usted":"se ","ustedes":"se "}
+    verb = verb[:-2]
+    reflex = True
+  else:
+    prefix = {"yo":"","tu":"","el":"","nosotros":"","ellos":"","ustedes":"","usted":""}
+    reflex = False
+  if verb[-2:] == "ír":
+    verb = verb[:-2]+"ir"
+  if tense == "present": 
+    for person in people:
+      print(person+": " + prefix[person] + present(verb, person))
+  if tense == "preterite": 
+    for person in people:
+      print(person+": " + prefix[person] + preterite(verb, person))
+  if tense == "imperfect": 
+    for person in people:
+      print(person+": " + prefix[person] + imperfect(verb, person))
+  if tense == "present perfect": 
+    for person in people:
+      print(person+": " + prefix[person] + presentPerfect(verb, person))
+  if tense == "past perfect": 
+    for person in people:
+      print(person+": " + prefix[person] + pastPerfect(verb, person))
+  if tense == "subjunctive": 
+    for person in people:
+      print(person+": " + prefix[person] + subjunctive(verb,person))
+  if tense == "future": 
+    for person in people:
+      print(person+": " + prefix[person] + future(verb,person))
+  if tense == "conditional": 
+    for person in people:
+      print(person+": " + prefix[person] + conditional(verb,person))
+  if tense == "imperfect subjunctive": 
+    for person in people:
+      print(person+": " + prefix[person] + imperfectSubjunctive(verb,person))
+  if tense == "affirmative imperative":
+    for person in imperativePeople:
+      print(person+": " + imperativePos(verb,person,reflex)+prefix[person][:-1])
+  if tense == "negative imperative":
+    for person in imperativePeople:
+      print(person+": no " + prefix[person] + imperativeNeg(verb,person))
